7851ebaead880eed7c5df717efac6bba988bfdb9
Shrutarv <shrutarv.awasthi@tu-dortmund.de>
2024-09-16 11:11:02 +0200

    Cleaning code and making it modular

--- Annotation/Annotation_rgb_ec/annotation_bbox_pose.py
+++ Annotation/Annotation_rgb_ec/annotation_bbox_pose.py
@@ -99,98 +99,17 @@ H_cam_vicon_2_cam_optical = np.array(data['H_cam_vicon_2_cam_optical'])
 H_cam1_2_rgb = np.array(data['H_cam1_2_rgb'])
 H_cam2_cam1 = np.array(data['H_cam2_cam1'])
 
-
-transformations = {}
+######### Here we save the transformations for the dataset. Annotations are not happening here. #########
 # Read the vicon coordinates of the even camera system. Traverse through the coordinates
 with open(json_path_camera_sys, 'r') as f:
     data = json.load(f)
-for i, v in data.items():
-    if i == str(len(data) - 1):
-        continue
-    translation = data[str(i)]['translation']
-    rotation_quat = data[str(i)]['rotation']
+save_transformations(data, H_cam_vicon_2_cam_optical, object_array, H_cam1_2_rgb, H_cam2_cam1)
 
-    # get rotation matrix from quaternion
-    rotation = R.from_quat(rotation_quat).as_matrix()
-    # make homogeneous transformation matrix
-    H_base_2_cam_vicon = np.eye(4)
-    H_base_2_cam_vicon[:3, :3] = rotation
-    H_base_2_cam_vicon[:3, 3] = translation
-
-    # make homogeneous transformation matrix from base to camera optical frame
-    H_base_2_cam_optical = np.matmul(H_base_2_cam_vicon, H_cam_vicon_2_cam_optical)
-
-    # invert H_vicon_2_cam_optical to get H_cam_optical_2_vicon
-    H_cam_optical_2_base = np.eye(4)
-    H_cam_optical_2_base[:3, :3] = np.transpose(H_base_2_cam_optical[:3, :3])
-    H_cam_optical_2_base[:3, 3] = -np.matmul(np.transpose(H_base_2_cam_optical[:3, :3]), H_base_2_cam_optical[:3, 3])
-    # Add H_cam_optical_2_base, timestamp to a dictionary. Append the H_cam_optical_2_base and timestamp on every iteration.
-    # This will give a list of dictionaries with H_cam_optical_2_base and timestamp
-    t_x = object_array[str(v['timestamp'])]['translation'][0]
-    t_y = object_array[str(v['timestamp'])]['translation'][1]
-    t_z = object_array[str(v['timestamp'])]['translation'][2]
-    r_x = object_array[str(v['timestamp'])]['rotation'][0]
-    r_y = object_array[str(v['timestamp'])]['rotation'][1]
-    r_z = object_array[str(v['timestamp'])]['rotation'][2]
-    r_w = object_array[str(v['timestamp'])]['rotation'][3]
-    rotation = R.from_quat([r_x, r_y, r_z, r_w]).as_matrix()
-
-    '''H_v_2_point = np.array([[1, 0, 0, t_x],
-                            [0, 1, 0, t_y],
-                            [0, 0, 1, t_z],
-                            [0, 0, 0, 1]])'''
-
-    H_v_2_point = np.eye(4)
-    H_v_2_point[:3, :3] = rotation
-    H_v_2_point[:3, 3] = [t_x, t_y, t_z]
-
-    H_cam_optical_2_point = np.matmul(H_cam_optical_2_base, H_v_2_point)
-    t_cam_optical_2_point = H_cam_optical_2_point[:3, 3]
-    r_cam_optical_2_point = H_cam_optical_2_point[:3, :3]
-    H_base_2_cam_optical = np.matmul(H_base_2_cam_vicon, H_cam_vicon_2_cam_optical)
-    # invert H_vicon_2_cam_optical to get H_cam_optical_2_vicon
-    H_cam_optical_2_base = np.eye(4)
-    H_cam_optical_2_base[:3, :3] = np.transpose(H_base_2_cam_optical[:3, :3])
-    H_cam_optical_2_base[:3, 3] = -np.matmul(np.transpose(H_base_2_cam_optical[:3, :3]), H_base_2_cam_optical[:3, 3])
-    # Compute translation t_cam_optical_2_base
-    t_cam_optical_2_base = H_cam_optical_2_base[:3, 3]
-    # t_cam_optical_2_base = np.transpose(H_base_2_cam_optical[:3, :3])
-    H_rgb_2_point = H_cam_optical_2_point
-    # project point (x,y,z) in cam0 coordinate to cam1 coordinate
-    point_cam0 = np.array([
-        [1, 0, 0, t_cam_optical_2_base[0]],
-        [0, 1, 0, t_cam_optical_2_base[1]],
-        [0, 0, 1, t_cam_optical_2_base[2]],
-        [0, 0, 0, 1]])
-
-    H_cam1_2_point = np.matmul(H_cam1_2_rgb, H_rgb_2_point)
-
-    t_cam1_2_point = H_cam1_2_point[:3, 3]
-    #print(t_cam1_2_point)
-    # project point (863,819) in cam1 coordinate to cam2 coordinate
-    H_cam2_2_point = np.matmul(H_cam2_cam1, H_cam1_2_point)
-    t_cam2_2_point = H_cam2_2_point[:3, 3]
-    #print(t_cam2_2_point)
-    transformations[str(data[str(i)]['timestamp'])] = {'H_cam_optical_2_base': H_cam_optical_2_base.tolist(),
-                                                       'H_cam_optical_2_point': H_cam_optical_2_point.tolist(),
-                                                       'H_base_2_cam_vicon': H_base_2_cam_vicon.tolist(),
-                                                       't_cam_optical_2_point': t_cam_optical_2_point.tolist(),
-                                                       't_cam_optical_2_base': t_cam_optical_2_base.tolist(),
-                                                       'point_event_cam_left': t_cam1_2_point.tolist(),
-                                                       'point_event_cam_right': t_cam2_2_point.tolist(),
-                                                       'rotation': rotation.tolist(),
-                                                       'timestamp': str(v['timestamp'])
-                                                       }
-
-with open('/home/eventcamera/data/transformations/transformations.json', 'w') as json_file:
-    json.dump(transformations, json_file, indent=2)
-print('saved transformations data')
-
+################## ANNOTATIONS #################
 count = 0
 with open('/home/eventcamera/data/transformations/transformations.json', 'r') as file:
     projected_point_rgb_ec1_ec2 = json.load(file)
-kr = {}
-vr = {}
+
 timestamp_closest_ec_right = sorted(timestamp_closest_ec_right)
 timestamp_closest_ec_left = sorted(timestamp_closest_ec_left)
 rgb_timestamp = sorted(rgb_timestamp)
@@ -199,17 +118,10 @@ for (kr, vr), (k, v) in zip(rotations_with_timestamps.items(), translations_with
     print(kr)
     rgb_t = rgb_timestamp[count]
     ec_left = timestamp_closest_ec_left[count]
-    #ec_left = timestamp_closest_ec_left_vicon[count]
     ec_right = timestamp_closest_ec_right[count]
-    #ec_right = timestamp_closest_ec_right_vicon[count]
     rgb_img_path = rgb_image_path + str(rgb_t) + ".png"
     event_cam_left = path_event_cam_left_img + str(ec_left) + ".png"
     event_cam_right = path_event_cam_right_img + str(ec_right) + ".png"
-    H_v_2_point = np.array([
-        [1, 0, 0, v[0]],
-        [0, 1, 0, v[1]],
-        [0, 0, 1, v[2]],
-        [0, 0, 0, 1]])
 
     t_cam_optical_2_point = np.array(projected_point_rgb_ec1_ec2[str(k)]['t_cam_optical_2_point'])
     H_cam_optical_2_point = np.array(projected_point_rgb_ec1_ec2[str(k)]['H_cam_optical_2_point'])
@@ -229,69 +141,14 @@ for (kr, vr), (k, v) in zip(rotations_with_timestamps.items(), translations_with
     vertices = np.array(trimesh_object.vertices) / 1000
 
     # translate the object to match the center with the vicon camera frame
-    if object_id == 2:
-        translation_vector = np.array([-0.05, 0, 0])
-        vertices -= translation_vector
-        points_3d -= translation_vector
-
-    if object_id == 3:
-        translation_vector = np.array([-0.05, 0, 0])
-        vertices -= translation_vector
-        points_3d -= translation_vector
-
-    if object_id == 4:
-        translation_vector = np.array([0, 0, 0])
-        vertices -= translation_vector
-        points_3d -= translation_vector
-        rotation_matrix = R.from_euler('z', 90, degrees=True).as_matrix()
-        vertices = np.dot(vertices, rotation_matrix)
-        points_3d = np.dot(points_3d, rotation_matrix)
-
-    if object_id == 6:
-        translation_vector = np.array([0, 0, 0.03])
-        #translation_vector = np.array([0, 0.05, 0])
-        vertices -= translation_vector
-        points_3d -= translation_vector
-
-    if object_id == 8:
-        translation_vector = np.array([0, 0, 0])
-        vertices -= translation_vector
-        points_3d -= translation_vector
-
-    if object_id == 9:
-        rotation_matrix = R.from_euler('z', 90, degrees=True).as_matrix()
-        vertices = np.dot(vertices, rotation_matrix)
-        points_3d = np.dot(points_3d, rotation_matrix)
-        translation_vector = np.array([0, 0.05, 0])
-        vertices -= translation_vector
-        points_3d -= translation_vector
-
-    if object_id == 10:
-        translation_vector = np.array([0, 0, 0])
-        vertices -= translation_vector
-        points_3d -= translation_vector
+    vertices, points_3d = get_translated_points_vertice(object_id, vertices, points_3d)
 
     object_3d_transform_points = np.matmul(H_cam_optical_2_point, np.vstack((points_3d.T, np.ones(points_3d.shape[0]))))[:3, :].T
     object_3d_transform_vertices = np.matmul(H_cam_optical_2_point, np.vstack((vertices.T, np.ones(vertices.shape[0]))))[:3, :].T
     center_3d = np.mean(object_3d_transform_points, axis=0)
 
     #################################### Exporting bounding box and pose values to a json file ####################################
-    # compute xmin, xmax, ymin, ymax, zmin, zmax
-    xmin = np.min(object_3d_transform_points[:, 0])
-    xmax = np.max(object_3d_transform_points[:, 0])
-    ymin = np.min(object_3d_transform_points[:, 1])
-    ymax = np.max(object_3d_transform_points[:, 1])
-    zmin = np.min(object_3d_transform_points[:, 2])
-    zmax = np.max(object_3d_transform_points[:, 2])
-    # Save above values in an array
-    Bbox = np.array([xmin, xmax, ymin, ymax, zmin, zmax])
-
-    # append the Bbox values to a json file row wise
-    file = os.path.join(output_dir, "bounding_box_labels.json")
-    with open(file, 'a') as json_file:
-        json.dump(Bbox.tolist(), json_file)
-        json_file.write('\n')
-
+    save_bbox_values(output_dir, object_3d_transform_points)
     # Save pose values to a json file
     rotation = H_cam_optical_2_point[:3, :3]
     rotmat = R.from_matrix(rotation)
@@ -318,7 +175,7 @@ for (kr, vr), (k, v) in zip(rotations_with_timestamps.items(), translations_with
         img_test = cv2.circle(img_test, tuple(point[0].astype(int)), 8, (0, 0, 255), -1)
     img_test = cv2.circle(img_test, tuple(center_2d.astype(int)), 8, (0, 0, 255), -1)
 
-    ############ Even camera 1 ############
+    ############ Event camera 1 ############
     t_cam1_2_point = np.array(projected_point_rgb_ec1_ec2[str(k)]['point_event_cam_left'])
     H_cam1_2_point = np.eye(4)
     H_cam1_2_point[:3, :3] = rotation
@@ -346,7 +203,7 @@ for (kr, vr), (k, v) in zip(rotations_with_timestamps.items(), translations_with
     for point in klt_2d_vertices:
         img_test1 = cv2.circle(img_test1, tuple(point[0].astype(int)), 3, (0, 0, 255), -1)
 
-    ############ Even camera 2 ############
+    ############ Event camera 2 ############
     t_cam2_2_point = np.array(projected_point_rgb_ec1_ec2[str(k)]['point_event_cam_right'])
     H_cam2_2_point = np.eye(4)
     H_cam2_2_point[:3, :3] = rotation
@@ -375,7 +232,7 @@ for (kr, vr), (k, v) in zip(rotations_with_timestamps.items(), translations_with
     for point in klt_2d_vertices:
         img_test2 = cv2.circle(img_test2, tuple(point[0].astype(int)), 3, (0, 0, 255), -1)
 
-    ####### Display the images ########
+    ########### Display the images ###########
     img_test = cv2.resize(img_test, (568, 426))
     img_test1 = cv2.resize(img_test1, (568, 426))
     img_test2 = cv2.resize(img_test2, (568, 426))
